191f85c450fa92bead84e9d608a6183719ef66c1
Rafae Vinicius Barros <rafaelviniciusbarros5@gmail.com>
2022-11-17 12:39:52 -0300

    Criação da classe de repositório para curso.

--- api/index.php
+++ api/index.php
@@ -14,3 +14,58 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 header('Access-Control-Allow-Origin: https://localhost:5500');
 header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
+
+function path()
+{
+    $dir = dirname($_SERVER['PHP_SELF']);
+    $caminho = str_replace([$dir, '/api'], '', $_SERVER['REQUEST_URI']);
+    return $caminho;
+}
+
+if (preg_match('/^(\/)?login/i', path())) {
+}
+
+if (preg_match('/^(\/)?avisos/i', path())) {
+
+    $avisoControladora = new AvisoControladora();
+
+    if (!$avisoControladora->visao->findByUrgencia() && !$sessao->logado()) {
+        return;
+    }
+
+    if (!($avisoControladora->init()
+    )) {
+        http_response_code(404);
+        die('Não encontrado.');
+    }
+    return;
+}
+if (preg_match('/^(\/)?setores/i', path()) && $sessao->logado()) {
+    $setorControladora = new SetorControladora();
+    if (!($setorControladora->init()
+    )) {
+        http_response_code(404);
+        die('Não encontrado.');
+    }
+    return;
+}
+if (preg_match('/^(\/)?usuarios/i', path()) && $sessao->logado()) {
+    $usuarioControladora = new UsuarioControladora();
+
+    if (!($usuarioControladora->init()
+    )) {
+        http_response_code(404);
+        die('Não encontrado.');
+    }
+    return;
+}
+
+if (preg_match('/^(\/)?configuracao/i', path())) {
+    $configuracaoControladora = new configuracaoControladora();
+    if (!($configuracaoControladora->init()
+    )) {
+        http_response_code(404);
+        die('Não encontrado.');
+    }
+    return;
+}
---
+++ front-end/app/src/nota/nota.ts
@@ -0,0 +1,16 @@
+export class AlunoCurso {
+   constructor(
+      public matricula: number,
+      public notaAv1: number,
+      public notaAv2: number, 
+      public notaF: number
+   ) { }
+
+   validar () : string[]{
+      const problemas : string[] = [];
+
+      if(this.notaAv1 instanceof number) problemas.push('Por favor, informe o nome.');
+      if(this.telefone.length < 9) problemas.push('Por favor, infomrar o telefone.');
+      return problemas;
+   }
+}
