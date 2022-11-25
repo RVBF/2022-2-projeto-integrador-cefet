6d540a944bfb98f6b0979a94c69f8357c880b602
rafa55565 <rafael_viniciusbarros@hotmail.com>
2022-11-25 09:46:17 -0300

    Criação da estrutura de conexão com o banco de dados.

---
+++ api/app/src/config/servidor-config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  Informações do servidor.
+ *
+ *  @author	Rafael Vinicius Barros Ferreira
+ * */
+ return [
+	'connections' => [
+		'mysql' => [
+			 'driver' => 'mysql',
+			//  'url' => env('DATABASE_URL'),
+			 'host' => getenv('MYSQL_DB_HOST'),
+			 'port' => getenv('MYSQL_DOCKER_PORT'),
+			 'database' => getenv('MYSQL_DB_DATABASE'),
+			 'username' => getenv('MYSQL_DB_USER'),
+			 'password' => getenv('MYSQL_DB_PASSWORD'),
+			 'charset' => getenv('MYSQL_CHARSET'),
+			 'collation' => getenv('MYSQL_COLLATION'),
+		],
+  ],
+ ]
+
+?>
