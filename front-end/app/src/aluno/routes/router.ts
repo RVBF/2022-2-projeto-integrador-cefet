fff9feb2c8d3e639b403681090ca10e94b505d8d
vitorhugorangel <vitorhugo.rangel@yahoo.com.br>
2022-11-30 17:07:14 -0300

    Refatora os testes do front e back-end
    Ajusta métodos de aluno e nota
    
    Remove arquivos redundantes
    
    Adiciona arquivos .sql

--- front-end/app/src/aluno/routes/router.ts
+++
@@ -1,6 +0,0 @@
-import { url } from './parameters.routes';
-
-export function router(pathRouter: string) {
-  const locationHash = url(window.location.hash);
-  return locationHash === pathRouter;
-}
