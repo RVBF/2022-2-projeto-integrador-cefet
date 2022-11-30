7e6b6246022c8b5341d5a96c0e9ee1cfda589866
rafa55565 <rafael_viniciusbarros@hotmail.com>
2022-11-30 19:43:12 -0300

    Adiciona pastas pages e routes em alunos.

---
+++ front-end/app/src/aluno/pages/Listar/index.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { Navigator } from '../../../components/Navigator';
+import { Alunos } from './components/Alunos';
+
+
+export async function Listar(root: HTMLDivElement) {
+  const main = document.createElement('main');
+  const content = document.createElement('div');
+
+  // Atributos
+  main.setAttribute('class', 'main-content-game');
+
+  const add = Navigator('Salvar', 'salvar');
+
+  // Atributos
+  content.setAttribute('class', 'content');
+
+  const list = Alunos([
+    { username: 'Name 1', id: '1' },
+    { username: 'Name 2', id: '2' },
+    { username: 'Name 3', id: '3' },
+  ]);
+
+  // Filhos
+  content.appendChild(list);
+  content.appendChild(add);
+
+  main.appendChild(content);
+
+  // Montar
+  root.appendChild(main);
+}
