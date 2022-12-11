1ffbf596fb5d1adf4d14c2bf75a374ace9632e8a
vitorhugorangel <vitorhugo.rangel@yahoo.com.br>
2022-12-11 15:16:48 -0300

    Ajusta classe login no back-end

--- api/app/src/login/login-repositorio-mysql.php
+++ api/app/src/login/login-repositorio-mysql.php
@@ -1,56 +1,60 @@
 <?php
-  require 'app/src/login/login-repositorio.php';  
-  require 'app/src/login/sessao-usuario.php';  
-  
-  class LoginRepositorioMysql implements LoginRepositorio{    
-    private $sessaoEmArquivo;
-    public function __construct ( &$pdo ) {
-      $this->pdo = $pdo;
-      $this->sessaoEmArquivo = new SessaoEmArquivo();
-    }
+require 'app/src/login/login-repositorio.php';
+require 'app/src/login/sessao-usuario.php';
 
-    function autenticar( $dadosLogin ) {
-      try{
-        $todosErros = $dadosLogin->validar();
+class LoginRepositorioMysql implements LoginRepositorio
+{
+  private $sessaoEmArquivo;
+  public function __construct(&$pdo)
+  {
+    $this->pdo = $pdo;
+    $this->sessaoEmArquivo = new SessaoEmArquivo();
+  }
 
-        if( count($todosErros) > 0 ){
-          echo implode(', ', $todosErros);
-          http_response_code(400);  
-          die();
-        }
+  function autenticar($dadosLogin)
+  {
+    try {
+      $todosErros = $dadosLogin->validar();
 
-        $preparedStatement = $this->pdo->prepare( 'SELECT id, nome, sobrenome, setor_id, perfil FROM responsavel WHERE `login` = :l AND senha = :s' );
-        $preparedStatement->execute( [ 'l' => $dadosLogin->login, 's' => $dadosLogin->senhaComHash() ] );
-        if( $preparedStatement->rowCount() < 1 ) {
-          echo json_encode(['error' => 'Login ou senha incorretos!']);
-          http_response_code(400);  
-          die();
-        }
-
-        $result = $preparedStatement->fetch( PDO::FETCH_ASSOC );
-        $usuarioSessao =  new SessaoUsuario( intval($result['id']), $result['nome'], $result['sobrenome'], $result['perfil'], intval($result['setor_id']) );
-
-        $sessaoFormatada = $usuarioSessao->sessaoFormatada();
-
-        $this->sessaoEmArquivo->regerarId();
-        $this->sessaoEmArquivo->definirValor( 'usuario', json_encode($sessaoFormatada)  );
-        return $sessaoFormatada;
-      } catch( Exception $e ) {
-        throw new RepositorioExcecao( $e->getMessage(), $e->getCode(), $e );
+      if (count($todosErros) > 0) {
+        echo implode(', ', $todosErros);
+        http_response_code(400);
+        die();
       }
-    }
 
-    function deslogar() {
-      try{
-        if( !$this->sessaoEmArquivo->logado() ){
-          http_response_code(400);
-          echo 'Usuário não está logado!';
-          die();
-        }
-
-        $this->sessaoEmArquivo->destruirSessao();
-      } catch( Exception $e ) {
-        throw new RepositorioExcecao( $e->getMessage(), $e->getCode(), $e );
+      $preparedStatement = $this->pdo->prepare('SELECT id, nome, sobrenome, setor_id, perfil FROM responsavel WHERE `login` = :l AND senha = :s');
+      $preparedStatement->execute(['l' => $dadosLogin->login, 's' => $dadosLogin->senhaComHash()]);
+      if ($preparedStatement->rowCount() < 1) {
+        echo json_encode(['error' => 'Login ou senha incorretos!']);
+        http_response_code(400);
+        die();
       }
+
+      $result = $preparedStatement->fetch(PDO::FETCH_ASSOC);
+      $usuarioSessao =  new SessaoUsuario(intval($result['id']), $result['nome'], $result['sobrenome'], $result['perfil'], intval($result['setor_id']));
+
+      $sessaoFormatada = $usuarioSessao->sessaoFormatada();
+
+      $this->sessaoEmArquivo->regerarId();
+      $this->sessaoEmArquivo->definirValor('usuario', json_encode($sessaoFormatada));
+      return $sessaoFormatada;
+    } catch (Exception $e) {
+      throw new RepositoryException($e->getMessage(), $e->getCode(), $e);
     }
   }
+
+  function deslogar()
+  {
+    try {
+      if (!$this->sessaoEmArquivo->logado()) {
+        http_response_code(400);
+        echo 'Usuário não está logado!';
+        die();
+      }
+
+      $this->sessaoEmArquivo->destruirSessao();
+    } catch (Exception $e) {
+      throw new RepositoryException($e->getMessage(), $e->getCode(), $e);
+    }
+  }
+}
