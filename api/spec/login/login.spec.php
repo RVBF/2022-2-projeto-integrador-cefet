b0bd76a811570fa0611f1c7a5d4d6c761692ad9e
vitorhugorangel <vitorhugo.rangel@yahoo.com.br>
2022-12-21 10:47:25 -0300

    Corrige testes back-end

--- api/spec/login/login.spec.php
+++ api/spec/login/login.spec.php
@@ -1,23 +1,24 @@
 <?php
 
+use App\Src\Login\Login;
+
 require 'vendor/autoload.php';
-require 'app/src/login/login.php';
+require_once 'app/src/login/login.php';
 
-describe( 'Login', function() {
+describe('Login', function () {
 
-    it( 'Deve validar login', function() {
-        $loginTest = new Login( 1, 'teste_login', 'teste_senha' );
-        expect( $loginTest->validar() )->toBe( [] );
-    } );
+    it('Deve validar login', function () {
+        $loginTest = new Login('teste_login', 'teste_senha');
+        expect($loginTest->validar())->toBe([]);
+    });
 
-    it( 'Não deve validar login quando nome do usuário não estiver definido', function() {
-        $loginTest = new Login( 1, '', 'teste_senha' );
-        expect( $loginTest->validar() )->toBe( ['Login não definido!'] );
-    } );
+    it('Não deve validar login quando nome do usuário não estiver definido', function () {
+        $loginTest = new Login('', 'teste_senha');
+        expect($loginTest->validar())->toBe(['Login não definido!']);
+    });
 
-    it( 'Não deve validar login quando não estiver definida', function() {
-        $loginTest = new Login( 1, 'teste_login', '' );
-        expect( $loginTest->validar() )->toBe( ['Senha não definida!'] );
-    } );
-
+    it('Não deve validar login quando não estiver definida', function () {
+        $loginTest = new Login('teste_login', '');
+        expect($loginTest->validar())->toBe(['Senha não definida!']);
+    });
 });
